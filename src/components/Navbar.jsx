974f09b6c55c91b67f10ca79c2ed2e9d0861c67b
cUserd <zabralalg@abv.bg>
2023-05-14 18:52:12 +0300

    navbar commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,67 @@ import { navLinks } from '../constants';
 import { logo,menu,close } from '../assets';
 
 const Navbar = () => {
+
+const [active,setActive] = useState('');
+const [toggle, setToggle] = useState(false);
+
   return (
     <nav
     className={`${styles.paddingX} w-full flex
-    items-center py-5 fixed top-0 z-20
+    items-center py-5 fixed top-0 z-20 
     bg-primery`}>
+        <div className='w-full flex justify-between
+        items-center max-w-7xl mx-auto'>
+          <Link
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0,0)
+          }}
+          >
+             <img src={logo} alt='logo' className='w-20
+             h-20 object-contain'/>
+             <p className='text-white text-[18px]
+             font-bold cursor-pointer'>Tsvetomir Draganov<span
+             className='sm:block hidden'> |
+              Java Mastery</span></p>
+          </Link>
+          <ul className='list-none hidden sm:flex 
+          flex-row gap-10'>
+            {navLinks.map((Link) => (
+              <li
+              key={Link.id}
+              className={`${
+                active === Link.title
+                ? 'text-white'
+                : 'text-secondary'
+              } hoover:text-white text=[18px]
+              font-medium cursor-pointer`}
+              onClick={()=> setActive(Link.title)}
+              >
+                <a href={`#${Link.id}`}>{Link.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div className='sm:hidden flex flex-1
+          justify-end items center'>
+              <img
+                src = {toggle ? close : menu}
+                alt = 'menu'
+                className='w-[28px] h-[28px]
+                object-contain cursor-pointer'
+                onClick={()=> setToggle(!toggle)}
+              />
 
+              <div className={`${!toggle ? 'hidden'
+              : 'flex'} p-6 black gradient absolute
+              top-20 right-0 mx-4 my-2 min-w-[140px]
+              z-10 rounded-xl `}>
+
+              </div>
+          </div>
+        </div>
     </nav>
   )
 }
