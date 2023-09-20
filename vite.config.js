59c9307a84f1456711ec5f94aab04f1ed1e5368d
DraganovTs <cvetomir_draganov@abv.bg>
2023-09-20 19:54:58 +0300

    size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // Adjust this value as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Define manual chunks as needed based on your application's structure
+          // For example, you can manually group modules into chunks
+          // 'chunk-name': ['module1', 'module2', ...],
+        },
+      },
+    },
+  },
+});
